5b78587b81525e9ef2e88632070ab9a31893260f
Galterdev <prevkiki77@gmail.com>
2024-07-19 17:38:44 +0200

    Add files via upload

---
+++ translation.js
@@ -0,0 +1,46 @@
+const translate = new Object();
+
+translate.player_id_label = 'ID:'
+
+translate.player_id_icon = '<i class="fa-solid fa-id-badge"></i>'
+
+
+translate.cash_label = 'CASH:'
+translate.bank_label = 'BANCA:'
+translate.black_money_label = 'SOLDI SPORCHI:'
+translate.job_label = 'LAVORO:'
+translate.gang_label = 'FAZIONI:'
+
+translate.cash_icon = '<i class="fa-solid fa-money-bill-wave"></i>'
+translate.bank_icon = '<i class="fa-solid fa-building-columns"></i>'
+translate.black_money_icon = '<i class="fa-solid fa-sack-dollar"></i>'
+translate.company_money_icon = '<i class="fa-solid fa-building"></i>'
+translate.job_icon = '<i class="fas fa-suitcase"></i>'
+translate.gang_icon = '<i class="fas fa-mask"></i>'
+translate.ammo_icon = '<i class="fa-solid fa-gun"></i>'
+
+translate.kmh = 'KM/H'
+translate.mph = 'MP/H'
+
+
+translate.customization_hud_title = 'MENU CUSTOM HUD'
+
+translate.help_draggable = 'Pour changer la position du hud, faites-le simplement glisser et changez.'
+
+translate.customization_vertial_name = 'Direction de état vertical'
+translate.customization_horizontal_name = 'Direction de état horizontal'
+translate.customization_no_stack_name = 'Statut sans pile'
+translate.customization_health_name = 'Santé'
+translate.customization_armor_name = 'Armure'
+translate.customization_stamina_name = 'Endurance'
+translate.customization_hunger_name = 'Faim'
+translate.customization_thirst_name = 'Soif'
+translate.customization_stress_name = 'Stress'
+translate.customization_voice_name = 'Chat vocal'
+translate.customization_speedometer_scale_name = 'Échelle du compteur de vitesse'
+translate.customization_speedometer_linear_name = 'Compteur de vitesse linéaire'
+translate.customization_speedometer_circle_name = 'Cercle du compteur de vitesse'
+translate.customization_minimap_circle_name = 'Mini-carte en forme de cercle'
+translate.customization_minimap_square_name = 'Mini-carte en forme de rectangle'
+translate.customization_cinematic_name = 'Mode cinématique'
+translate.customization_reset_name = 'REMETTRE LES PARAMÈTRES DE DÉFAULT'
